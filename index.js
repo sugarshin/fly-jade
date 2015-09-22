74fd3a412158f76db0d8f858f0b94838e8a433b1
Hal Henke <halhenke@gmail.com>
2015-09-22 21:17:48 +1000

    prepend options.base to options.filename if provided

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function() {
   this.filter("jade", (data, options) => ({
-    css: require("jade").render(data.toString(), options), ext: ".html"
+    css: require("jade").render(data.toString(), {...options, filename: options.base ? `${options.base}/${options.filename}` : options.filename}), ext: ".html"
   }))
 }
